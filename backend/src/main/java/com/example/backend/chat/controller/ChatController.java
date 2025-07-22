a40a66c120f850b2be4a0a2483e6e6faafc662e2
bluetree2 <kmsid77@gmail.com>
2025-07-22 17:24:35 +0900

    필요없는 코드 삭제

--- backend/src/main/java/com/example/backend/chat/controller/ChatController.java
+++ backend/src/main/java/com/example/backend/chat/controller/ChatController.java
@@ -2,6 +2,7 @@ package com.example.backend.chat.controller;
 
 import com.example.backend.chat.dto.ChatForm;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
@@ -20,10 +21,20 @@ public class ChatController {
 
     @MessageMapping("/chat/private")
     public void sendPrivateMessage(ChatForm msg, Principal principal) {
+        System.out.println("principal " + principal);
+        System.out.println("msg " + msg);
         template.convertAndSendToUser(
-                msg.getTo(),
-                "/queue/messages",
-                msg
+                msg.getTo(),                // 받는 사용자 ID
+                "/queue/messages",          // 구독 경로
+                msg                         // 메시지 payload
         );
     }
+
+
+   /* @MessageMapping("/chat/send")
+    @SendTo("/topic/public")
+    public void ChatMessage broadcast(ChatMessage msg) {
+        // 받은 메시지를 그대로 /topic/public 구독자 모두에게 전송
+        return msg;
+    }*/
 }
