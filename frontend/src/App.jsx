1fa56114a83081ead14b2cd166f5c71c1f34f97c
kikyungchan <kikyungchan@gmail.com>
2025-07-22 14:56:17 +0900

    나브바 작업중

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import { ProductRegist } from "./feature/Product/ProductRegist.jsx";
 import NavBar from "./feature/common/NavBar.jsx";
-import { Navbar } from "react-bootstrap";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
+      <NavBar />
       <Routes>
         <Route path="product/regist" element={<ProductRegist />} />
       </Routes>
