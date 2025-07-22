214d49d6b1b7e5e58347ca3650b1a5d949992ae1
bluetree2 <kmsid77@gmail.com>
2025-07-22 17:24:08 +0900

    주석으로 설명 추가

--- frontend/src/chat/Chat.jsx
+++ frontend/src/chat/Chat.jsx
@@ -3,48 +3,60 @@ import { Client } from "@stomp/stompjs";
 import SockJS from "sockjs-client";
 // import { Stomp } from "stompjs";
 
-const WS_URL = "http://localhost:8080/ws-chat";
+const WS_URL = "http://localhost:5173/ws-chat";
 const SEND_DEST = "/app/chat/private";
 const SUBSCRIBE_DEST = "/user/queue/messages";
 
 export function Chat({ username }) {
-  const [target, setTarget] = useState("");
-  const [text, setText] = useState("");
-  const [msgs, setMsgs] = useState([]);
-  const clientRef = useRef(null);
+  const [target, setTarget] = useState(""); //수신자 id
+  const [text, setText] = useState(""); // 보낼 텍스트
+  const [msgs, setMsgs] = useState([]); // 주고 받은 메시지들
+  const clientRef = useRef(null); // STOMP 인스턴스 담아 둘 상자
 
   useEffect(() => {
     const client = new Client({
-      webSocketFactory: () => new SockJS(WS_URL),
+      webSocketFactory: () => new SockJS(WS_URL), // SockJS 연결
       debug: (str) => console.log(str),
-      reconnectDelay: 5000,
+      reconnectDelay: 5000, // 끊기면 5초후 재연결
     });
     client.onConnect = () => {
+      // 연결 성공 시
+      console.log("연결됨!");
       client.subscribe(SUBSCRIBE_DEST, (message) => {
+        console.log("받음");
+        // 서버의 json 메시지를 파싱해서 msgs 배열에 추가
         setMsgs((prev) => [...prev, JSON.parse(message.body)]);
       });
     };
+
+    // 연결 활성화(connect 시도)
     client.activate();
+    // 훅 박에서도 쓰기 위해 ref에 저장
     clientRef.current = client;
-    console.log("연결됨!");
+
+    // 언마운트 될 때
     return () => {
-      client.deactivate();
+      client.deactivate(); //연결 해제
     };
   }, []);
 
   const sendMessage = () => {
-    if (!text.trim()) return;
-    const chatMsg = { from: username, content: text };
+    if (!text.trim()) return; // 값 업승면 그냥 반환
+    // 보낼 메시지 객체
+    const chatMsg = { from: username, to: target, message: text };
+    // SEND_DEST로 파일 전송
     clientRef.current.publish({
       destination: SEND_DEST,
       body: JSON.stringify(chatMsg),
     });
-    setText("");
+    setText(""); // 입력창 초기화
   };
 
   return (
     <div style={{ padding: 20, fontFamily: "sans-serif" }}>
       <h4>1:1 상담 ({username})</h4>
+
+      {/*채팅 로그*/}
       <div
         style={{
           border: "1px solid #ddd",
