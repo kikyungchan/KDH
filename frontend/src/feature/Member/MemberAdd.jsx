faba60c1aec537de5dea900aec4253f2a21b3a8b
JaehongLIM2 <nu0044@naver.com>
2025-07-23 10:29:17 +0900

    회원 등록시 정규식에 맞도록 입력가능하세

--- frontend/src/feature/Member/MemberAdd.jsx
+++ frontend/src/feature/Member/MemberAdd.jsx
@@ -4,6 +4,7 @@ import {
   FormControl,
   FormGroup,
   FormLabel,
+  FormText,
   Row,
   Spinner,
 } from "react-bootstrap";
@@ -11,6 +12,15 @@ import { useState } from "react";
 import axios from "axios";
 
 export function MemberAdd() {
+  // 입력 항목 정규식
+  const loginIdRegEx = /^[A-Za-z][A-Za-z0-9]{3,19}$/;
+  const emailRegEx =
+    /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
+  const passwordRegEx = /^[A-Za-z0-9]{8,20}$/;
+  // const nameRegEx = /^[가-힣]{2,5}$/; // 한글 이름만
+  const nameRegEx = /^[가-힣a-zA-Z\s]{2,20}$/; // 한글 + 영문 이름 허용 시
+  const phoneRegEx = /^01[016789][0-9]{7,8}$/;
+
   // 오늘 날짜를 yyyy-MM-dd 형식으로 반환하는 함수
   const getToday = () => {
     const today = new Date();
@@ -19,6 +29,8 @@ export function MemberAdd() {
     const dd = String(today.getDate()).padStart(2, "0");
     return `${yyyy}-${mm}-${dd}`;
   };
+
+  // 로그인 입력 항목
   const [loginId, setLoginId] = useState("");
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
@@ -26,8 +38,33 @@ export function MemberAdd() {
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
   const [address, setAddress] = useState("");
-  // 초기값은 오늘 날짜
-  const [birthday, setBirthday] = useState(getToday());
+  const [birthday, setBirthday] = useState(getToday()); // 초기값은 오늘 날짜
+
+  // 정규식과 일치하는지
+  const [loginIdValid, setLoginIdValid] = useState(true);
+  const [passwordValid, setPasswordValid] = useState(true);
+  const [nameValid, setNameValid] = useState(true);
+  const [phoneValid, setPhoneValid] = useState(true);
+  const [emailValid, setEmailValid] = useState(true);
+
+  // 가입 버튼 클릭 여부
+  const [isSubmiited, setIsSubmiited] = useState(false);
+
+  // 각 항목을 입력하지 않으면 가입 버튼 비활성화
+  const requiredFields = [
+    loginId,
+    password,
+    name,
+    birthday,
+    phone,
+    email,
+    address,
+  ];
+  const allFieldsFilled = requiredFields.every((field) => field.trim() !== "");
+
+  // password 와 password2(비밀번호 확인)이 일치하지 않으면 가입버튼 비활성화
+  const passwordConfirm = password === password2;
+  const disabled = !allFieldsFilled || !passwordConfirm;
 
   function handleSignUpClick() {
     axios
@@ -61,10 +98,20 @@ export function MemberAdd() {
             <FormControl
               type="text"
               value={loginId}
+              placeholder="아이디는 영문으로 시작하며 4~20자, 영문+숫자 조합만 가능합니다."
+              autoComplete="username"
               onChange={(e) => {
                 setLoginId(e.target.value);
+                setLoginIdValid(loginIdRegEx.test(e.target.value));
               }}
             />
+            {loginId !== "" && (
+              <FormText className={loginIdValid ? "text-muted" : "text-danger"}>
+                {loginIdValid
+                  ? "아이디는 영문으로 시작하며 4~20자, 영문+숫자 조합만 가능합니다."
+                  : "유효한 아이디 형식이 아닙니다."}
+              </FormText>
+            )}
           </FormGroup>
         </div>
         <div>
@@ -73,23 +120,33 @@ export function MemberAdd() {
             <FormControl
               type="password"
               value={password}
+              placeholder="비밀번호를 입력하세요"
               onChange={(e) => {
                 setPassword(e.target.value);
+                setPasswordValid(passwordRegEx.test(e.target.value));
               }}
             />
+            <FormText className="text-danger" hidden={passwordValid}>
+              유효한 비밀번호 형식이 아닙니다.
+            </FormText>
           </FormGroup>
         </div>
         <div>
-          {/* TODO : 비밀번호 일치 여부 */}
           <FormGroup>
             <FormLabel>비밀번호 확인</FormLabel>
             <FormControl
               type="password"
               value={password2}
+              placeholder="비밀번호 확인"
               onChange={(e) => {
                 setPassword2(e.target.value);
               }}
             />
+            {password2 && password !== password2 && (
+              <div style={{ color: "red", fontSize: "0.875rem" }}>
+                비밀번호가 일치하지 않습니다.
+              </div>
+            )}
           </FormGroup>
         </div>
         <div>
@@ -98,10 +155,16 @@ export function MemberAdd() {
             <FormControl
               type="text"
               value={name}
+              placeholder="이름을 입력하세요"
+              autoComplete="name"
               onChange={(e) => {
                 setName(e.target.value);
+                setNameValid(nameRegEx.test(e.target.value));
               }}
             />
+            <FormText className="text-danger" hidden={nameValid}>
+              유효한 이름 형식이 아닙니다.
+            </FormText>
           </FormGroup>
         </div>
         <div>
@@ -110,6 +173,7 @@ export function MemberAdd() {
             <FormControl
               type="date"
               value={birthday}
+              autoComplete="bday"
               onChange={(e) => {
                 setBirthday(e.target.value);
               }}
@@ -122,10 +186,16 @@ export function MemberAdd() {
             <FormControl
               type="text"
               value={phone}
+              placeholder="전화번호를 입력하세요"
+              autoComplete="tel"
               onChange={(e) => {
                 setPhone(e.target.value);
+                setPhoneValid(phoneRegEx.test(e.target.value));
               }}
             />
+            <FormText className="text-danger" hidden={phoneValid}>
+              유효한 전화번호 형식이 아닙니다.
+            </FormText>
           </FormGroup>
         </div>
         <div>
@@ -134,10 +204,16 @@ export function MemberAdd() {
             <FormControl
               type="text"
               value={email}
+              placeholder="이메일을 입력하세요"
+              autoComplete="email"
               onChange={(e) => {
                 setEmail(e.target.value);
+                setEmailValid(emailRegEx.test(e.target.value));
               }}
             />
+            <FormText className="text-danger" hidden={emailValid}>
+              유효한 이메일 형식이 아닙니다.
+            </FormText>
           </FormGroup>
         </div>
         <div>
@@ -146,6 +222,8 @@ export function MemberAdd() {
             <FormControl
               type="text"
               value={address}
+              placeholder="주소를 입력하세요"
+              autoComplete="address-line1"
               onChange={(e) => {
                 setAddress(e.target.value);
               }}
@@ -153,7 +231,9 @@ export function MemberAdd() {
           </FormGroup>
         </div>
         <div>
-          <Button onClick={handleSignUpClick}>가입</Button>
+          <Button onClick={handleSignUpClick} disabled={disabled}>
+            가입
+          </Button>
         </div>
       </Col>
     </Row>
