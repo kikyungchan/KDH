1cdf22d7f9ce7b0cf95b20cd35754739cbba86e9
kikyungchan <kikyungchan@gmail.com>
2025-07-25 09:35:23 +0900

    장바구니 버튼 모달 기본css / 계속쇼핑버튼 setmodal false

--- frontend/src/feature/Product/ProductDetail.jsx
+++ frontend/src/feature/Product/ProductDetail.jsx
@@ -219,13 +219,52 @@ export function ProductDetail() {
         </Col>
       </Row>
       <Modal show={showModal} onHide={() => setShowModal(false)} centered>
-        <Modal.Body className="text-center" style={{ padding: "40px 20px 0" }}>
+        <Modal.Body
+          className="text-center d-flex justify-content-center align-items-center"
+          style={{
+            height: "130px",
+            fontSize: "14px",
+            padding: "0",
+          }}
+        >
           <p style={{ marginBottom: "0", fontSize: "16px" }}>
             선택하신 상품을 장바구니에 담았습니다
           </p>
         </Modal.Body>
-        <Button variant="secondary">계속쇼핑</Button>
-        <Button variant="primary">장바구니</Button>
+        <div
+          style={{
+            display: "flex",
+            borderTop: "1px solid #ddd",
+            borderBottomLeftRadius: "10px",
+            borderBottomRightRadius: "10px",
+            overflow: "hidden",
+          }}
+        >
+          <button
+            onClick={() => setShowModal(false)}
+            style={{
+              flex: 1,
+              padding: "12px 0",
+              border: "none",
+              background: "white",
+              fontWeight: "bold",
+              borderRight: "1px solid #ddd",
+            }}
+          >
+            계속쇼핑
+          </button>
+          <button
+            style={{
+              flex: 1,
+              padding: "12px 0",
+              border: "none",
+              background: "white",
+              fontWeight: "bold",
+            }}
+          >
+            장바구니
+          </button>
+        </div>
       </Modal>
     </Container>
   );
