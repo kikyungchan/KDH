3f5cedd4111876ceb79160ca3541168a004dd838
JaehongLIM2 <nu0044@naver.com>
2025-07-29 10:46:59 +0900

    role 테이블 추가

--- sql/Member.sql
+++ sql/Member.sql
@@ -15,3 +15,25 @@ DROP TABLE member;
 
 ALTER TABLE member
     MODIFY COLUMN id INT AUTO_INCREMENT;
+
+# 권한 테이블
+CREATE TABLE role
+(
+    name VARCHAR(30) PRIMARY KEY
+);
+
+
+CREATE TABLE member_role
+(
+    member_id INT         NOT NULL,
+    role_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (member_id, role_name),
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (role_name) REFERENCES role (name)
+);
+
+INSERT INTO member_role
+    (member_id, role_name)
+VALUES (139, 'admin');
+
+
