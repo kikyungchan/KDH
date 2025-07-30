6b8ce5c89f28faaec0655a3402b08b05842015f0
kikyungchan <kikyungchan@gmail.com>
2025-07-30 16:48:53 +0900

    NoticeSection.jsx 수정

--- frontend/src/feature/Product/ProductDetail/NoticeSection.jsx
+++ frontend/src/feature/Product/ProductDetail/NoticeSection.jsx
@@ -1,23 +1,36 @@
 import { Col, Row } from "react-bootstrap";
 import noticeData from "./notice.json";
 
-function NoticeSection() {
+function NoticeSection({ style }) {
   return (
-    <div style={{ marginTop: "35px" }}>
-      <h2 className="mt-5">NOTICE</h2>
-      <Row>
-        <Col>
-          {noticeData.sections.map((section, idx) => (
-            <div key={idx}>
-              <hr />
-              {section.title && <Col className="fw-bold">{section.title}</Col>}
-              {section.items.map((item, i) => (
-                <Col key={i}>{item}</Col>
-              ))}
-            </div>
-          ))}
-        </Col>
-      </Row>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "75%",
+        marginLeft: "180px",
+        ...style,
+      }}
+    >
+      <div style={{ marginTop: "35px" }}>
+        <h2 className="mt-5">NOTICE</h2>
+        <Row>
+          <Col>
+            {noticeData.sections.map((section, idx) => (
+              <div key={idx}>
+                <hr />
+                {section.title && (
+                  <Col className="fw-bold">{section.title}</Col>
+                )}
+                {section.items.map((item, i) => (
+                  <Col key={i}>{item}</Col>
+                ))}
+              </div>
+            ))}
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 }
