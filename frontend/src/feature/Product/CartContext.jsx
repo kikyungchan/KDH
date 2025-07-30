158bcc5623475fc6eaecbb9544882c07618d54a4
kikyungchan <kikyungchan@gmail.com>
2025-07-30 10:33:52 +0900

    navbar 아이콘추가 밑 링크연동 검색창작업중

---
+++ frontend/src/feature/Product/CartContext.jsx
@@ -0,0 +1,34 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      axios
+        .get("api/product/cart", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((res) => setCartCount(res.data.length))
+        .catch(() => setCartCount(0));
+    } else {
+      const guestCart = JSON.parse(localStorage.getItem("guestCart") || "[]");
+      setCartCount(guestCart.length);
+    }
+  }, []);
+  return (
+    <CartContext.Provider value={{ cartCount, setCartCount }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  return useContext(CartContext);
+}
--- frontend/src/feature/common/NavBar.jsx
+++ frontend/src/feature/common/NavBar.jsx
@@ -1,24 +1,87 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { Link } from "react-router";
 import { AuthenticationContext } from "./AuthenticationContextProvider.jsx";
+import {
+  FiChevronLeft,
+  FiSearch,
+  FiShoppingCart,
+  FiUser,
+} from "react-icons/fi";
+import "./Navbar.css";
 
 function NavBar(props) {
   const { user, isAdmin } = useContext(AuthenticationContext);
+  const [showSearch, setShowSearch] = useState(false);
+  const searchRef = useRef(null);
+
+  // 검색창 아이콘 한번더 누르거나 바깥영역누르면 검색창닫히도록
+  useEffect(() => {
+    function handleClickOutside(e) {
+      if (searchRef.current && !searchRef.current.contains(e.target)) {
+        setShowSearch(false);
+      }
+    }
+
+    if (showSearch) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showSearch]);
 
   return (
-    <nav>
-      <Link to="/">메인</Link>
-      <Link to="/product/list">상품목록</Link>
-      <Link to="/product/regist">상품등록</Link>
-      <Link to="/product/cart">장바구니</Link>
-      {user !== null && isAdmin && <Link to="/member/list">회원목록</Link>}
-      {user === null && <Link to="/signup">회원가입</Link>}
-      {user === null && <Link to="/login">로그인</Link>}
-      {user !== null && <Link to="/logout">로그아웃</Link>}
-      {user !== null && <Link to={`/member?id=${user.id}`}>{user.name}</Link>}
-      <br />
-      <Link to="/chat/chatting">채팅 프로토콜</Link>
-    </nav>
+    <>
+      <nav className="navbar-container">
+        {/* 왼쪽 메뉴 */}
+        <div className="navbar-left">
+          <Link to="/">메인</Link>
+          <Link to="/product/list">상품목록</Link>
+          <Link to="/product/regist">상품등록</Link>
+          {user !== null && isAdmin && <Link to="/member/list">회원목록</Link>}
+          {user === null && <Link to="/signup">회원가입</Link>}
+          {user === null && <Link to="/login">로그인</Link>}
+          {user !== null && <Link to="/logout">로그아웃</Link>}
+          {user !== null && (
+            <Link to={`/member?id=${user.id}`}>{user.name}</Link>
+          )}
+          <Link to="/chat/chatting">채팅 프로토콜</Link>
+        </div>
+
+        {/* 오른쪽 아이콘 */}
+        <div className="navbar-icons">
+          <FiSearch
+            className="navbar-icon"
+            onClick={() => setShowSearch((prev) => !prev)}
+            style={{ cursor: "pointer" }}
+          />
+          <Link to={user ? `/member?id=${user.id}` : "/login"}>
+            <FiUser className="navbar-icon" />
+          </Link>
+          <Link to="/product/cart" className="cart-icon-wrapper">
+            <FiShoppingCart className="navbar-icon" />
+          </Link>
+        </div>
+      </nav>
+      {/* 돋보기 눌렀을떄 나오는 검색창 */}
+      <div
+        ref={searchRef}
+        className={`search-bar-wrapper ${showSearch ? "active" : ""}`}
+      >
+        <FiChevronLeft
+          className="search-close-icon"
+          onClick={() => setShowSearch(false)}
+        />
+        <input
+          type="text"
+          placeholder="키워드로 검색"
+          className="search-input"
+        />
+      </div>
+    </>
   );
 }
 
