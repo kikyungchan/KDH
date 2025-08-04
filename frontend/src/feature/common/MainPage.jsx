8a1ddebc8c197208ff77c7f09b6209034de1d8e3
kikyungchan <kikyungchan@gmail.com>
2025-08-04 12:44:32 +0900

    cover슬라이드 page 외에도 마우스휠 불가능한 오류 수정

--- frontend/src/feature/common/MainPage.jsx
+++ frontend/src/feature/common/MainPage.jsx
@@ -1,17 +1,21 @@
 import NavBar from "./NavBar.jsx";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import { useEffect } from "react";
 
 function MainPage() {
-  useEffect(() => {
-    // 메인 페이지에서만 body의 스크롤을 막기
-    document.body.style.overflow = "hidden";
+  const location = useLocation();
 
-    // 컴포넌트가 unmount될 때 스크롤을 원상태로 복원
+  useEffect(() => {
+    if (location.pathname === "/") {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
     return () => {
       document.body.style.overflow = "auto";
     };
-  }, []);
+  }, [location.pathname]);
   return (
     <>
       <NavBar />
