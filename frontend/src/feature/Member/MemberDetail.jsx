4663fec8bdbb20a0fa1e84649a747db0eefdafac
JaehongLIM2 <nu0044@naver.com>
2025-08-05 15:02:09 +0900

    회원정보 수정 페이지 css

--- frontend/src/feature/Member/MemberDetail.jsx
+++ frontend/src/feature/Member/MemberDetail.jsx
@@ -11,10 +11,10 @@ import {
   Row,
   Spinner,
 } from "react-bootstrap";
-import { useContext, useEffect, useState } from "react";
-import { useNavigate, useSearchParams } from "react-router";
+import {useContext, useEffect, useState} from "react";
+import {useNavigate, useSearchParams} from "react-router";
 import axios from "axios";
-import { AuthenticationContext } from "../common/AuthenticationContextProvider.jsx";
+import {AuthenticationContext} from "../common/AuthenticationContextProvider.jsx";
 import WithDrawModal from "./Modal/WithDrawModal.jsx";
 import WithdrawModal from "./Modal/WithDrawModal.jsx";
 
@@ -31,7 +31,7 @@ export function MemberDetail() {
 
   const navigate = useNavigate();
 
-  const { logout, hasAccess } = useContext(AuthenticationContext);
+  const {logout, hasAccess} = useContext(AuthenticationContext);
 
   // 회원 정보 조회
   useEffect(() => {
@@ -40,8 +40,10 @@ export function MemberDetail() {
       .then((res) => {
         setMember(res.data);
       })
-      .catch((err) => {})
-      .finally(() => {});
+      .catch((err) => {
+      })
+      .finally(() => {
+      });
   }, [memberParams]);
 
   // 회원 정보 없을때 (ex: null)
@@ -49,7 +51,7 @@ export function MemberDetail() {
     return (
       <div>
         <div>
-          <Spinner />
+          <Spinner/>
         </div>
         회원 정보를 불러오는 중 . . .{" "}
       </div>
@@ -63,7 +65,7 @@ export function MemberDetail() {
 
     axios
       .delete(`/api/member`, {
-        data: { id: member.id, oldPassword: oldPassword },
+        data: {id: member.id, oldPassword: oldPassword},
       })
       .then((res) => {
         navigate("/");
@@ -88,119 +90,124 @@ export function MemberDetail() {
       <div className="flex justify-center items-start pt-10">
         <div className="w-full max-w-[600px] mx-auto px-4">
           <div className="px-8 py-6 shadow rounded-2xl bg-white">
-              <Row>
-                <Col>
-                  <h2 className="mb-6 text-center text-2xl font-semibold">회원 정보</h2>
+            <Row>
+              <Col>
+                <h2 className="mb-6 text-center text-2xl font-bold">회원 정보</h2>
+                <div className="space-y-4">
+                  {/* 아이디 */}
+                  <div className="flex items-center gap-4 mb-4">
+                    <label htmlFor="loginId" className="label w-24  block mr-6">아이디</label>
+                    <input
+                      type="text"
+                      id="loginId"
+                      readOnly
+                      value={member.loginId}
+                      className=" input input-bordered px-3 py-2 ml-1"
+                    />
+                  </div>
 
-                  <div className="space-y-4">
-                    {/* 아이디 */}
-                    <div>
-                      <label className="block font-semibold mb-1">아이디</label>
-                      <input
-                        type="text"
-                        readOnly
-                        value={member.loginId}
-                        className="w-full rounded px-3 py-2 bg-gray-100 cursor-not-allowed"
-                      />
-                    </div>
+                  {/* 이름 */}
+                  <div className="flex items-center gap-4 mb-4">
+                    <label htmlFor="name" className="label w-24 font-semibold block mr-6">이름</label>
+                    <input
+                      type="text"
+                      id="name"
+                      readOnly
+                      value={member.name}
+                      className=" input input-bordered px-3 py-2 ml-1"
+                    />
+                  </div>
 
-                    {/* 이름 */}
-                    <div>
-                      <label className="block font-semibold mb-1">이름</label>
-                      <input
-                        type="text"
-                        readOnly
-                        value={member.name}
-                        className="w-full rounded px-3 py-2 bg-gray-100 cursor-not-allowed"
-                      />
-                    </div>
+                  {/* 생년월일 */}
+                  <div className="flex items-center gap-4 mb-4">
+                    <label htmlFor="birthDate" className="label w-24 font-semibold block mr-6">생년월일</label>
+                    <input
+                      type="text"
+                      id="birthDate"
+                      readOnly
+                      value={member.birthday}
+                      className=" input input-borderedpx-3 py-2 ml-1"
+                    />
+                  </div>
 
-                    {/* 생년월일 */}
-                    <div>
-                      <label className="block font-semibold mb-1">생년월일</label>
-                      <input
-                        type="text"
-                        readOnly
-                        value={member.birthday}
-                        className="w-full rounded px-3 py-2 bg-gray-100 cursor-not-allowed"
-                      />
-                    </div>
+                  {/* 전화번호 */}
+                  <div className="flex items-center gap-4 mb-4">
+                    <label htmlFor="phone" className="label w-24 font-semibold block mr-6">전화번호</label>
+                    <input
+                      type="text"
+                      id="phone"
+                      readOnly
+                      value={member.phone}
+                      className="input input-bordered px-3 py-2 ml-1"
+                    />
+                  </div>
 
-                    {/* 전화번호 */}
-                    <div>
-                      <label className="block font-semibold mb-1">전화번호</label>
-                      <input
-                        type="text"
-                        readOnly
-                        value={member.phone}
-                        className="w-full rounded px-3 py-2 bg-gray-100 cursor-not-allowed"
-                      />
-                    </div>
+                  {/* 이메일 */}
+                  <div className="flex items-center gap-4 mb-4">
+                    <label htmlFor="email" className="label w-24 font-semibold block mr-6">이메일</label>
+                    <input
+                      type="text"
+                      id="email"
+                      readOnly
+                      value={member.email}
+                      className="input input-bordered px-3 py-2 ml-1"
+                    />
+                  </div>
 
-                    {/* 이메일 */}
-                    <div>
-                      <label className="block font-semibold mb-1">이메일</label>
-                      <input
-                        type="text"
-                        readOnly
-                        value={member.email}
-                        className="w-full rounded px-3 py-2 bg-gray-100 cursor-not-allowed"
-                      />
-                    </div>
-
-                    {/* 주소 */}
-                    <div>
-                      <label className="block font-semibold mb-1">주소</label>
+                  {/* 주소 */}
+                  <div className="flex items-start gap-4 mb-4">
+                    <label htmlFor="email" className="label w-24 font-semibold block mr-6">주소</label>
+                    <div className="flex flex-col flex-1 gap-2">
                       <input
                         type="text"
                         readOnly
                         value={member.zipCode}
-                        className="w-full rounded px-3 py-2 bg-gray-100 cursor-not-allowed mb-2"
-                      />
+                        className=" input input-bordered px-3 py-2 mb-2"/>
                       <input
                         type="text"
                         readOnly
                         value={member.address}
-                        className="w-full rounded px-3 py-2 bg-gray-100 cursor-not-allowed mb-2"
-                      />
+                        className=" input input-bordered px-3 py-2 mb-2"/>
+
                       <input
                         type="text"
                         readOnly
                         value={member.addressDetail}
-                        className="w-full rounded px-3 py-2 bg-gray-100 cursor-not-allowed"
-                      />
+                        className=" input input-bordered px-3 py-2 mb-2"/>
+
                     </div>
                   </div>
+                </div>
 
-                  {/* 버튼 */}
-                  {hasAccess(member.loginId) && (
-                    <div className="text-right mt-6">
-                      <button
-                        className="btn btn-info px-4 py-2 rounded mr-2 hover:opacity-90 transition"
-                        onClick={() => navigate(`/member/edit?id=${member.id}`)}
-                      >
-                        수정
-                      </button>
-                      <button
-                        className="btn btn-error px-4 py-2 rounded mr-2 hover:opacity-90 transition"
-                        onClick={() => setWithdrawModalShow(true)}
-                      >
-                        탈퇴
-                      </button>
-                    </div>
-                  )}
-                </Col>
-                <WithdrawModal
-                  show={withdrawModalShow}
-                  onClose={handleCloseWithdrawModal}
-                  oldPassword={oldPassword}
-                  setOldPassword={setOldPassword}
-                  passwordError={passwordError}
-                  setPasswordError={setPasswordError}
-                  handleWithdrawButtonClick={handleWithdrawButtonClick}
-                  isWithdrawProcessing={isWithdrawProcessing}
-                />
-              </Row>
+                {/* 버튼 */}
+                {hasAccess(member.loginId) && (
+                  <div className="text-right mt-6">
+                    <button
+                      className="btn btn-info px-4 py-2 rounded mr-2 hover:opacity-90 transition"
+                      onClick={() => navigate(`/member/edit?id=${member.id}`)}
+                    >
+                      수정
+                    </button>
+                    <button
+                      className="btn btn-error px-4 py-2 rounded mr-2 hover:opacity-90 transition"
+                      onClick={() => setWithdrawModalShow(true)}
+                    >
+                      탈퇴
+                    </button>
+                  </div>
+                )}
+              </Col>
+              <WithdrawModal
+                show={withdrawModalShow}
+                onClose={handleCloseWithdrawModal}
+                oldPassword={oldPassword}
+                setOldPassword={setOldPassword}
+                passwordError={passwordError}
+                setPasswordError={setPasswordError}
+                handleWithdrawButtonClick={handleWithdrawButtonClick}
+                isWithdrawProcessing={isWithdrawProcessing}
+              />
+            </Row>
           </div>
         </div>
       </div>
