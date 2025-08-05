592eefa8fac7e51ed66337ca8fc5ac49157d8cfd
bluetree2 <kmsid77@gmail.com>
2025-08-05 17:04:23 +0900

    스크롤 막힌거 풀기 보완

--- frontend/src/feature/common/MainPage.jsx
+++ frontend/src/feature/common/MainPage.jsx
@@ -1,11 +1,14 @@
 import NavBar from "./NavBar.jsx";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import { useEffect } from "react";
 
 function MainPage() {
+  const location = useLocation();
+
   useEffect(() => {
     // 주소 path 가 / (메인페이지) 일 때만 스크롤 막게수정
-    if (location.pathname == "/") {
+    if (location.pathname === "/") {
+      console.log("path : ", location.pathname);
       // 메인 페이지에서만 body의 스크롤을 막기
       document.body.style.overflow = "hidden";
     }
@@ -14,7 +17,7 @@ function MainPage() {
     return () => {
       document.body.style.overflow = "auto";
     };
-  }, []);
+  }, [location.pathname]);
   return (
     <>
       <NavBar />
