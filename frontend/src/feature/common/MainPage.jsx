5829ff10fa1082e5b0310aac4b5fb10b4732e49a
bluetree2 <kmsid77@gmail.com>
2025-08-05 16:49:47 +0900

    스크롤 막힌거 풀기

--- frontend/src/feature/common/MainPage.jsx
+++ frontend/src/feature/common/MainPage.jsx
@@ -4,8 +4,11 @@ import { useEffect } from "react";
 
 function MainPage() {
   useEffect(() => {
-    // 메인 페이지에서만 body의 스크롤을 막기
-    document.body.style.overflow = "hidden";
+    // 주소 path 가 / (메인페이지) 일 때만 스크롤 막게수정
+    if (location.pathname == "/") {
+      // 메인 페이지에서만 body의 스크롤을 막기
+      document.body.style.overflow = "hidden";
+    }
 
     // 컴포넌트가 unmount될 때 스크롤을 원상태로 복원
     return () => {
