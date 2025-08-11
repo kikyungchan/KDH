8b0d97dffd4f7beb8f00c93027670bf8e4b24403
kikyungchan <kikyungchan@gmail.com>
2025-08-11 10:19:14 +0900

    guest_order_item에 참조하고있는 product_id가 존재하는 상품은 삭제 안되는 문제 수정

--- backend/src/main/java/com/example/backend/product/repository/GuestOrderItemRepository.java
+++ backend/src/main/java/com/example/backend/product/repository/GuestOrderItemRepository.java
@@ -1,9 +1,12 @@
 package com.example.backend.product.repository;
 
 import com.example.backend.product.entity.GuestOrderItem;
+import com.example.backend.product.entity.Product;
 import com.example.backend.product.entity.ProductOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GuestOrderItemRepository extends JpaRepository<GuestOrderItem, Integer> {
     void deleteByOption(ProductOption option);
+
+    void deleteByProduct(Product product);
 }
