95b3018c820c03fc0f3be53746c8794274014c8f
JaehongLIM2 <nu0044@naver.com>
2025-08-18 12:02:40 +0900

    주문시 발급되는 토큰 형식 변경 금일날짜 + "-" + 랜덤 12자리 ex)20250818-294919295817

--- backend/src/main/java/com/example/backend/product/controller/ProductController.java
+++ backend/src/main/java/com/example/backend/product/controller/ProductController.java
@@ -26,7 +26,10 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.format.DateTimeFormatter;
+
 import java.security.SecureRandom;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -42,16 +45,21 @@ public class ProductController {
     private final ProductRepository productRepository;
     private final ProductThumbnailRepository productThumbnailRepository;
     private final ProductOptionRepository productOptionRepository;
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyyMMdd");
 
     public class OrderTokenGenerator {
         private static final SecureRandom random = new SecureRandom();
 
         public static String generateToken() {
+            // 오늘 날짜(yyyyMMdd)
+            String datePart = LocalDate.now().format(DATE_FORMAT);
+
+            // 랜던 16자리 숫자
             StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < 16; i++) {
+            for (int i = 0; i < 12; i++) {
                 sb.append(random.nextInt(10));
             }
-            return sb.toString();
+            return datePart + "-" + sb.toString();
         }
     }
 
