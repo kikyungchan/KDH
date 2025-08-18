bb26bb120527d8664b433d82bea90d08b44d14b6
kikyungchan <kikyungchan@gmail.com>
2025-08-18 17:37:55 +0900

    주석 원복

--- frontend/src/feature/common/NavBar/NavLeft.jsx
+++ frontend/src/feature/common/NavBar/NavLeft.jsx
@@ -137,6 +137,35 @@ function NavLeft({ user, isAdmin, handleCategoryClick }) {
           상품등록
         </Link>
       )}
+      {/*{user !== null && isAdmin && (*/}
+      {/*  <Link to="/member/list" className="btn btn-ghost text-xl">*/}
+      {/*    회원목록*/}
+      {/*  </Link>*/}
+      {/*)}*/}
+      {/*{user === null && (*/}
+      {/*  <Link to="/signup" className="btn btn-ghost text-xl">*/}
+      {/*    회원가입*/}
+      {/*  </Link>*/}
+      {/*)}*/}
+      {/*{user && (*/}
+      {/*  <>*/}
+      {/*    <Link to="/logout" className="btn btn-ghost text-xl">*/}
+      {/*      로그아웃*/}
+      {/*    </Link>*/}
+      {/*    /!*<Link to={`/member?id=${user.id}`} className="btn btn-ghost text-xl">*!/*/}
+      {/*    /!*  {user.name}*!/*/}
+      {/*    /!*</Link>*!/*/}
+      {/*    <Link to="/qna/list" className="btn btn-ghost text-xl">*/}
+      {/*      문의 내역*/}
+      {/*    </Link>*/}
+      {/*  </>*/}
+      {/*)}*/}
+      {/*<Link to="/chat/chatting" className="btn btn-ghost text-xl">*/}
+      {/*  채팅 프로토콜*/}
+      {/*</Link>*/}
+      {/*<Link to="/pay/Checkout" className="btn btn-ghost text-xl">*/}
+      {/*  토스 페이먼츠*/}
+      {/*</Link>*/}
     </div>
   );
 }
