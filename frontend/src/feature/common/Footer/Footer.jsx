b3dbb713839f8acb52ada004b1651ae38c43309d
kikyungchan <kikyungchan@gmail.com>
2025-08-19 10:35:44 +0900

    푸터 레이아웃

--- frontend/src/feature/common/Footer/Footer.jsx
+++ frontend/src/feature/common/Footer/Footer.jsx
@@ -12,17 +12,33 @@ const Footer = () => {
           <div className="footer-brand">
             <div className="footer-logo">KDH</div>
             <div className="footer-links">
-              <a href="#">회사소개</a>
-              <a href="#">이용약관</a>
-              <a href="#">개인정보처리방침</a>
-              <a href="#">고객센터</a>
+              <a href="/Home">회사소개</a>
+              <a href="/Home">이용약관</a>
+              <a href="/Home">개인정보처리방침</a>
+              <a href="/Home">고객센터</a>
             </div>
           </div>
-          <div className="footer-info">
-            <div className="info-title">고객센터 운영시간</div>
-            <div className="info-text">평일 10:00 ~ 18:00</div>
-            <div className="info-text">(점심시간 12:00 ~ 13:00)</div>
-            <div className="info-text">주말 및 공휴일 휴무</div>
+          <div className="footer-right">
+            <div className="footer-info">
+              <div className="info-title">고객센터 운영시간</div>
+              <div className="info-text">평일 10:00 ~ 18:00</div>
+              <div className="info-text">(점심시간 12:00 ~ 13:00)</div>
+              <div className="info-text">주말 및 공휴일 휴무</div>
+            </div>
+            <div className="footer-sns">
+              <a href="/Home">
+                <img src="../../../../public/logo/insta.webp" alt="Instagram" />
+                Instagram
+              </a>
+              <a href="/Home">
+                <img src="../../../../public/logo/Youtube.png" alt="YouTube" />
+                YouTube
+              </a>
+              <a href="/Home">
+                <img src="../../../../public/logo/KakaoTalk.svg" alt="Kakao" />
+                Kakao
+              </a>
+            </div>
           </div>
         </div>
 
@@ -31,20 +47,6 @@ const Footer = () => {
           <div className="footer-copyright">
             <p>© 2025 KDH. All rights reserved.</p>
             <p>문의: support@kdhshop.com | 1234-5678</p>
-          </div>
-          <div className="footer-sns">
-            <a href="/Home" rel="noopener noreferrer">
-              Instagram
-              <img src="../../../../public/logo/insta.webp" alt="Instagram" />
-            </a>
-            <a href="/Home" rel="noopener noreferrer">
-              Youtube
-              <img src="../../../../public/logo/Youtube.png" alt="YouTube" />
-            </a>
-            <a href="/Home" rel="noopener noreferrer">
-              Kakao
-              <img src="../../../../public/logo/KakaoTalk.svg" alt="Kakao" />
-            </a>
           </div>
         </div>
       </div>
